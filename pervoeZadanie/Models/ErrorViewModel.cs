28d89f766359237ec4f3cb755a635f7a256f1f1b
xXxandry2xXx <112953841+xXxandry2xXx@users.noreply.github.com>
2023-09-20 21:00:42 +0500

    Добавьте файлы проекта.

---
+++ pervoeZadanie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pervoeZadanie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
